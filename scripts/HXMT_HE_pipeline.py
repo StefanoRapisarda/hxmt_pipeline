b781194464a66672acd7aaa27a35c8ffd4f7902c
StefanoRapisarda <60002042+StefanoRapisarda@users.noreply.github.com>
2021-01-28 05:36:05 +0100

    Update HXMT_HE_pipeline.py
    Added channels in output names of spectral analysis products

--- scripts/HXMT_HE_pipeline.py
+++ scripts/HXMT_HE_pipeline.py
@@ -261,7 +261,10 @@ for proposal in proposals:
               if spectra and rsp and spec_bkg:
                 total_spec_flag = check_HE_spectra_files('energy_spectra.txt','energy_spectra_bkg.txt','energy_spectra_rsp.txt',dir_name)
                 if total_spec_flag:
-                  outputs = [dir_name+'/total_spectrum.pi',dir_name+'/total_spectrum_bkg.pi',dir_name+'/total_spectrum_rsp.pi']
+                  spectral_names = ['total_spectrum','total_spectrum_bkg','total_spectrum_rsp']
+                  outputs = [dir_name+'/'+sp+'_CH{}_{}.pi'.format(minch,maxch) for sp in spectral_names]
+                  #outputs = [dir_name+'/total_spectrum.pi',
+                  #           dir_name+'/total_spectrum_bkg.pi',dir_name+'/total_spectrum_rsp.pi']
                   cmd = 'hhe_spec2pi {} {} {} {} {} {}'.\
                     format(dir_name+'/energy_spectra.txt',dir_name+'/energy_spectra_bkg.txt',dir_name+'/energy_spectra_rsp.txt',
                           outputs[0],outputs[1],outputs[2])
